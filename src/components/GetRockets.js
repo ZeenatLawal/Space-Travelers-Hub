cdb29280f64544e88537d434708f19eaf22d5fb8
Juliana Ifionu <julieifionu@gmail.com>
2021-10-04 23:33:41 +0100

    Fetch rockets data from API

---
+++ src/components/GetRockets.js
@@ -0,0 +1,20 @@
+// import { useDispatch } from 'react-redux';
+// import { setReservation } from '../redux/rockets/rockets';
+
+const getReservation = async () => {
+  const newResponse = [];
+  await fetch('https://api.spacexdata.com/v3/rockets')
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i <= data.length - 1; i += 1) {
+        const item = {};
+        item.rocket_name = data[i].rocket_name;
+        item.description = data[i].description;
+        item.flickr_images = data[i].flickr_images;
+        newResponse.push(item);
+      }
+    });
+  return newResponse;
+};
+
+export default getReservation;
