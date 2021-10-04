52a8f455210a1c990dc06776ee61b470f8c089ea
Juliana Ifionu <julieifionu@gmail.com>
2021-10-04 14:25:32 +0100

    Create Header component and render app logo

---
+++ src/Pages/Header.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      <h1>Space Traveler's Hub</h1>
+    </div>
+  )
+}
+
+export default Header;
