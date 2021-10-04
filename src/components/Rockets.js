80a14e94730ed8f9692f23a3af2fbf2600c8f0f0
Juliana Ifionu <julieifionu@gmail.com>
2021-10-04 23:37:45 +0100

    Get state from the state slice using useSelector and render rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,18 @@
-const Rockets = () => (
-  <div>
-    <h1>Hello</h1>
-  </div>
-);
+import { useSelector } from 'react-redux';
+import RocketList from './RocketList';
+
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rocketsReducer);
+
+  return (
+    <div>
+      <ul>
+        {rockets.map((rocket) => (
+          <RocketList rocketProps={rocket} key={rocket.id} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Rockets;
