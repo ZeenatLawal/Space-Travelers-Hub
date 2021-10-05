934769b08e852d04d75acfff50948ff02261a2c8
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-05 23:29:22 +0100

    Filter and display reserved missions

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Container from 'react-bootstrap/Container';
 
-const MyProfile = () => (
-  <div>
-    <h1>Under Construction</h1>
-  </div>
-);
+const MyProfile = () => {
+  const missions = useSelector((state) => state.missionsReducer.missions);
+
+  const filterReserved = missions.filter((mission) => mission.reserved);
+
+  return (
+    <Container>
+      <ul>
+        {filterReserved.map((mission) => (
+          <li className="border" key={mission.id}>{mission.name}</li>
+        ))}
+      </ul>
+      {!filterReserved && (
+        <h3>No Reserved Mission</h3>
+      )}
+    </Container>
+  );
+};
 export default MyProfile;
