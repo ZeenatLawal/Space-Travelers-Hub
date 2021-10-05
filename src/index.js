92ef2d32165c4f8a97383f2af83452eb2e3a0a6c
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-05 16:50:45 +0100

    Implement leave mission and update store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import store from './redux/configureStore';
 
 ReactDOM.render(
