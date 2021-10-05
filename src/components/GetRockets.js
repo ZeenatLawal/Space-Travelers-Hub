49d380443bfbcd8dcc8148868633935f66a3eab9
Juliana Ifionu <julieifionu@gmail.com>
2021-10-05 11:36:28 +0100

    Replace for loop with map

--- src/components/GetRockets.js
+++ src/components/GetRockets.js
@@ -1,17 +1,16 @@
 const getReservation = async () => {
-  const newResponse = [];
+  let result = [];
   await fetch('https://api.spacexdata.com/v3/rockets')
     .then((response) => response.json())
     .then((data) => {
-      for (let i = 0; i <= data.length - 1; i += 1) {
-        const item = {};
-        item.rocket_name = data[i].rocket_name;
-        item.description = data[i].description;
-        item.flickr_images = data[i].flickr_images;
-        newResponse.push(item);
-      }
+      result = data.map((rocket) => ({
+        id: rocket.id,
+        rocket_name: rocket.rocket_name,
+        description: rocket.description,
+        flickr_images: rocket.flickr_images,
+      }));
     });
-  return newResponse;
+  return result;
 };
 
 export default getReservation;
