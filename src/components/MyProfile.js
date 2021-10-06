9456480fc2a00ce14d49b1a105e0e8d08e2861bc
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-06 09:16:46 +0100

    Remove error for no mission joined

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import RocketProfile from './RocketProfile';
-import Container from 'react-bootstrap/Container';
 
 const MyProfile = () => {
   const missions = useSelector((state) => state.missionsReducer.missions);
 
-  const filterReserved = missions.filter((mission) => mission.reserved);
+  let filterReserved;
+  if (missions) {
+    filterReserved = missions.filter((mission) => mission.reserved);
+  }
 
   return (
-    <Container>
-      <ul>
-        {filterReserved.map((mission) => (
-          <li className="border" key={mission.id}>{mission.name}</li>
-        ))}
-      </ul>
-      {!filterReserved && (
-        <h3>No Reserved Mission</h3>
-      )}
-    </Container>
-    <div className="myProfile">
+    <div className="myProfile mt-4">
+      <div className="rocketProfileWrap me-5">
+        <h4 className="myRockets">My Missions</h4>
+        <ul className="myRocketLists">
+          {filterReserved && (
+            filterReserved.map((mission) => (
+              <li className="myRocketList" key={mission.id}>{mission.name}</li>
+            )))}
+        </ul>
+        {(!filterReserved || !filterReserved[0]) && (
+          <h5 className="noReservation">No mission yet!</h5>
+        )}
+      </div>
       <RocketProfile />
     </div>
   );
